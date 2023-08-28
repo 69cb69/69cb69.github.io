8022d6a50111d77443e4a2039e4c96a8405ea424
69cb69 <139967662+69cb69@users.noreply.github.com>
2023-08-28 18:36:32 +0800

    Update wx.js

--- wx.js
+++ wx.js
@@ -1,24 +1,26 @@
 /* function jump1(){
-  window.location.href="https://ajam.gakiurar.com/f/X5eoLoWoa1ym4a6";
+  window.location.href="https://m0q.oxtigdmq.com/f/X-2PWDeY869n61UZ";
 } */
 /* 改这条是改快手跳 */
 
 
 
 /* function jump11(){
-  window.location.href="https://polaris.zijieapi.com/luckycat/super_inviter/v1/invite_code?bCode=84W4S1&taskId=7140476324040175625";
+  window.location.href="https://youliao.com/upload/store/storeConfirmationPic/2023/202308/20230823/20230823165805141108.html?key=hX08nj&5er=zFrwWK2v";
 } */
-/* 改这条是改抖音跳 */
+/* 改这条是改快活跳 */
 
 
 
 /* for (var a = 0; a < t; a++) {
-  document.getElementsByClassName("aheadr1")[a].href = 'http://baidu.com'
-  document.getElementsByClassName("aheadr2")[a].href = 'http://baidu.com'
-  document.getElementsByClassName("aheadr3")[a].href = 'http://baidu.com'
+  document.getElementsByClassName("aheadr1")[a].href = 'https://xrfl-1318683004.cos-website.ap-nanjing.myqcloud.com'
+  document.getElementsByClassName("aheadr2")[a].href = 'https://p.qpic.cn/qqgameedu/0/f7745872706c773e53a9e7a51d09a8bc_0/0'
+  document.getElementsByClassName("aheadr3")[a].href = 'https://p.qpic.cn/qqgameedu/0/b561f0c4b3175e2b3ce16dd42a3bc4bf_0/0'
+  document.getElementsByClassName("aheadr3")[a].href = 'https://p.qpic.cn/qqgameedu/0/f5a9f7879d7474bfdae1213d43bdc159_0/0'
    
 }   */
 
 /* aheadr1 完整版后续
-aheadr2 土豪专属
-aheadr3 进群链接 */
+aheadr2 土豪专属1
+aheadr3 土豪专属2
+aheadr4 进群链接 */
