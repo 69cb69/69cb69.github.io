77c2d7938c7d81c958853d5c945e984def57e911
69cb69 <139967662+69cb69@users.noreply.github.com>
2023-08-28 18:46:16 +0800

    Add files via upload

---
+++ qq.js
@@ -0,0 +1,24 @@
+/* function jump1(){
+  window.location.href="https://m0q.oxtigdmq.com/f/X-2PWDeY869n61UZ";
+} */
+/* 改这条是改快手跳 */
+
+
+
+/* function jump11(){
+  window.location.href="http://d2mbm.xxbwb.com/c9";
+} */
+/* 改这条是改快活跳 */
+
+
+
+/* for (var a = 0; a < t; a++) {
+  document.getElementsByClassName("aheadr1")[a].href = 'https://p.qpic.cn/qqgameedu/0/c00612993c89f6338b097d4dc8883e74_0/0'
+  document.getElementsByClassName("aheadr2")[a].href = 'https://p.qpic.cn/qqgameedu/0/c50b181af444bd12a82ae654bdc44134_0/0'
+  document.getElementsByClassName("aheadr3")[a].href = 'https://xrfl-1318683004.cos-website.ap-nanjing.myqcloud.com'
+   
+}   */
+
+/* aheadr1 素材
+aheadr2 土豪专属
+aheadr3 往期回顾 */
