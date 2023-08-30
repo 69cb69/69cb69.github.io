f93eba4bfe6cc195d15d914d6abe95579d15c3ec
69cb69 <139967662+69cb69@users.noreply.github.com>
2023-08-30 09:28:46 +0800

    Update qq.js

--- qq.js
+++ qq.js
@@ -1,22 +1,21 @@
 /* function jump1(){
-  window.location.href="https://m0q.oxtigdmq.com/f/X-2PWDeY869n61UZ";
+  window.location.href="https://wr6s.zvzgptii.com/f/X-8Xda3FSHiCt1CY";
 } */
 /* 改这条是改快手跳 */
 
 
 
 /* function jump11(){
-  window.location.href="http://d2mbm.xxbwb.com/c9";
+  window.location.href="https://p.qpic.cn/qqgameedu/0/35f2bdadf7a915892b6dfed8d10094a1_0/0";
 } */
 /* 改这条是改快活跳 */
 
 
 
-/* for (var a = 0; a < t; a++) {
-  document.getElementsByClassName("aheadr1")[a].href = 'https://p.qpic.cn/qqgameedu/0/74f97f72b8c542590d3d3a523c19611e_0/0'
-  document.getElementsByClassName("aheadr2")[a].href = 'https://xrfl-1318683004.cos-website.ap-nanjing.myqcloud.com'
-   
-}   */
+for (var a = 0; a < t; a++) {
+  document.getElementsByClassName("aheadr1")[a].href = 'https://p.qpic.cn/qqgameedu/0/35f2bdadf7a915892b6dfed8d10094a1_0/0'
+  document.getElementsByClassName("aheadr2")[a].href = 'https://xrfl1-1318683004.cos-website.ap-nanjing.myqcloud.com'   
+}  
 
-/* aheadr1 土豪专属
+/* aheadr1 土豪专属1
 aheadr2 往期回顾 */
