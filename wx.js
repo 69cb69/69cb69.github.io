b34dcaaee5468ef1f49a21189e8505c48d70150a
69cb69 <139967662+69cb69@users.noreply.github.com>
2023-08-30 09:01:42 +0800

    Update wx.js

--- wx.js
+++ wx.js
@@ -13,14 +13,12 @@
 
 
 /* for (var a = 0; a < t; a++) {
-  document.getElementsByClassName("aheadr1")[a].href = 'https://xrfl-1318683004.cos-website.ap-nanjing.myqcloud.com'
+  document.getElementsByClassName("aheadr1")[a].href = 'www.baidu.com'
   document.getElementsByClassName("aheadr2")[a].href = 'https://p.qpic.cn/qqgameedu/0/f7745872706c773e53a9e7a51d09a8bc_0/0'
   document.getElementsByClassName("aheadr3")[a].href = 'https://p.qpic.cn/qqgameedu/0/b561f0c4b3175e2b3ce16dd42a3bc4bf_0/0'
-  document.getElementsByClassName("aheadr3")[a].href = 'https://p.qpic.cn/qqgameedu/0/f5a9f7879d7474bfdae1213d43bdc159_0/0'
    
 }   */
 
-/* aheadr1 完整版后续
-aheadr2 土豪专属1
-aheadr3 土豪专属2
-aheadr4 进群链接 */
+/* aheadr1 土豪专属1
+aheadr2 土豪专属2
+aheadr3 进群链接 */
