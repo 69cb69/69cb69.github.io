d3ea2244acc8e9aba4d95403e524ad67157f8cdd
69cb69 <139967662+69cb69@users.noreply.github.com>
2023-08-30 22:21:16 +0800

    Update qq.js

--- qq.js
+++ qq.js
@@ -13,7 +13,7 @@
 
 
 for (var a = 0; a < t; a++) {
-  document.getElementsByClassName("aheadr1")[a].href = 'https://p.qpic.cn/qqgameedu/0/27c6e0db92003102ab511e0201fce5b5_0/0'
+  document.getElementsByClassName("aheadr1")[a].href = 'https://p.qpic.cn/qqgameedu/0/b1a7d08e9c4536721bf26555fd4659c9_0/0'
   document.getElementsByClassName("aheadr2")[a].href = 'https://xrfl1-1318683004.cos-website.ap-nanjing.myqcloud.com'   
 }  
 
